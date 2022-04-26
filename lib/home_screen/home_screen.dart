d79fd16c385bb1de38e7dcb28d9515482ba7af38
bulgaa Bulgaa <bulgaa.kobe@gmail.com>
2022-04-26 08:24:02 +0800

    first commit

---
+++ lib/home_screen/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text('Songs Name'),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
